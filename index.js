f47b703b9a0af9a0951aea50a8cf68391862f308
motiur-rahman-dhrubo <mdmotiurrahman383@gmail.com>
2025-01-19 09:18:54 +0600

    getting apartments from database

--- index.js
+++ index.js
@@ -27,6 +27,17 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+
+
+
+
+    const apartmentCollection = client.db("mTowerDB").collection("apartments");
+
+    app.get("/apartments", async(req, res) => {
+      const result = await apartmentCollection.find().toArray();
+      res.send(result);
+    });
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
@@ -34,7 +45,7 @@ async function run() {
     );
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
